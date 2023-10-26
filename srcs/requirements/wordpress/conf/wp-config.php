7be973055b175fdd705bc03fde756d19cd0049f6
Tanguy EDELIN <tedelin@student.42.fr>
2023-10-26 15:17:35 +0200

    in progress

---
+++ srcs/requirements/wordpress/conf/wp-config.php
@@ -0,0 +1,7 @@
+<?php
+define('DB_NAME', getenv('DB_NAME'));
+define('DB_USER', getenv('DB_USER'));
+define('DB_PASSWORD', getenv('DB_PASSWORD'));
+define('DB_HOST', getenv('DB_HOST'));
+define('DB_CHARSET', 'utf8');
+define('DB_COLLATE', '');
